6d139add223183a9ac1a9583d10f93aad2f7b3c0
Thibaut Nguyen <thibautnguyen@gmail.com>
2017-05-19 12:10:46 +0200

    Sync animation with lights

---
+++ src/dmxConstants.ts
@@ -0,0 +1,6 @@
+export const DMX_CONSTANTS = {
+    BLINDER_ON: [144, 1, 127],
+    BLINDER_OFF: [144, 1, 0],
+    BLACKOUT_ON: [144, 0, 127],
+    BLACKOUT_OFF: [144, 0, 0]
+};
--- src/index.ts
+++ src/index.ts
@@ -2,13 +2,15 @@ import {Observable, Scheduler} from 'rxjs';
 import {defaultGameState, GameState} from './types/gameState';
 import {pixiApp} from './renderer';
 import {keyboard$} from './observables/keyboard';
-import {midiInputs$, midiInputTriggers$} from './observables/midi';
+import {midiInputs$, midiInputTriggers$, midiOutput$, midiOutSubject$} from './observables/midi';
 import MIDIInput = WebMidi.MIDIInput;
 import '../node_modules/skeleton-css/css/skeleton.css';
 import './styles/index.css';
 import './styles/fire.css';
 import {getGraphicTypeSelection} from './observables/graphicTypeSelector';
 import {mutateGameState} from './state/index';
+import MIDIOutput = WebMidi.MIDIOutput;
+import {DMX_CONSTANTS} from './dmxConstants';
 
 const TICKER_INTERVAL = 17;
 const ticker$ = Observable
@@ -24,20 +26,51 @@ const ticker$ = Observable
         })
     );
 
-const graphicMapping$ = midiInputs$.flatMap(midiInputs =>
-    getGraphicTypeSelection(midiInputs, document.querySelector('.sidebar'))
-);
+const graphicMapping$ = Observable.combineLatest(midiInputs$, midiOutput$).flatMap(([midiInputs, midiOutpus]) => {
+    return getGraphicTypeSelection(midiInputs, document.querySelector('.sidebar'));
+});
 
 const midi$ = Observable.merge(keyboard$, midiInputTriggers$);
 
-pixiApp.init(document.querySelector('.fireplace'), defaultGameState);
 const gameLoop$ = ticker$.combineLatest(midi$, graphicMapping$)
+    .do(([ticker, midiNotes, graphicMapping]) => {
+        midiOutSubject$.next({midiNotes, graphicMapping});
+    })
     .scan((state: GameState, [ticker, midiNotes, graphicMapping]) =>
             mutateGameState(state, midiNotes, ticker, graphicMapping)
         , defaultGameState);
 
+// PixiApp
+pixiApp.init(document.querySelector('.fireplace'), defaultGameState);
 // Gameloop
 gameLoop$
     .subscribe((gameState: GameState) => {
         pixiApp.render(gameState);
-});
+    });
+
+// DMX Lights
+const MIDI_OUT_DEVICE = 'IAC Driver IAC Bus 4';
+midiOutSubject$
+    .withLatestFrom(midiOutput$)
+    .throttleTime(50)
+    .subscribe(([notesMapping, midiOutputs]) => {
+        // when midi notes id match mapping with a redirect
+        // send these to a hardcoded midi output
+        const notesToSend = notesMapping.midiNotes
+            .filter(n => notesMapping.graphicMapping
+                .find(m => m.inputId === n.inputId && m.redirectOutput)
+            );
+
+        const output = midiOutputs.find(o => o.name === MIDI_OUT_DEVICE);
+        if (!output) {
+            return;
+        }
+
+        if (notesToSend.length) {
+            output.send(DMX_CONSTANTS.BLINDER_ON);
+            output.send(DMX_CONSTANTS.BLACKOUT_OFF);
+        } else {
+            output.send(DMX_CONSTANTS.BLINDER_OFF);
+            output.send(DMX_CONSTANTS.BLACKOUT_ON);
+        }
+    });
