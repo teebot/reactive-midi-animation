aa69a98f77bcb5efd232b629a4e13b073e35837e
Thibaut Nguyen <thibautnguyen@gmail.com>
2017-05-21 00:36:35 +0200

    Video background

--- src/renderer.ts
+++ src/renderer.ts
@@ -5,6 +5,15 @@ import Graphics = PIXI.Graphics;
 let graphicsByType: Map<string, Array<Array<Graphics>>>;
 const app = new Application(800, 600, {backgroundColor: 0x000000});
 
+const video = document.createElement('video');
+video.preload = 'auto';
+video.loop = true;
+video.src = require('./assets/retroscape.mp4');
+const texture = PIXI.Texture.fromVideo(video);
+const videoSprite = new PIXI.Sprite(texture);
+videoSprite.width = app.renderer.width;
+videoSprite.height = app.renderer.height;
+
 /**
  * initialises all possible graphics based visible the default game state
  * @param canvasDomContainer
@@ -13,6 +22,9 @@ const app = new Application(800, 600, {backgroundColor: 0x000000});
 const init = (canvasDomContainer: Element, defaultGameState: GameState): void => {
     canvasDomContainer.appendChild(app.view);
     canvasDomContainer.querySelector('canvas').addEventListener('dblclick', fullscreenHandler);
+
+
+    app.stage.addChild(videoSprite);
 
     // initial graphics flattened from initial game state
     // each graphic type is assigned an array of raw pixiJS graphic objects
@@ -24,6 +36,7 @@ const init = (canvasDomContainer: Element, defaultGameState: GameState): void =>
     for (const graphicsGroup of graphicsByType.values()) {
         graphicsGroup.forEach(gg => gg.forEach(g => app.stage.addChild(g)));
     }
+
 };
 
 const fullscreenHandler = (): void => {
